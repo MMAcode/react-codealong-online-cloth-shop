1a46d236577d67e06d3be571005d326af4ced024
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-02-07 22:58:18 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-codealong-online-cloth-shop/precache-manifest.9022707edf163208878b66182d32fad9.js"
+  "/react-codealong-online-cloth-shop/precache-manifest.3e2cba981f0dbff6e7751a88ade9cb20.js"
 );
 
 self.addEventListener('message', (event) => {
