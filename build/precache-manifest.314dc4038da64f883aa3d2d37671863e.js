284f554a3e8c0e85b5b86cd0f331058a1fcc3ef4
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-03-23 10:04:33 +0000

    gh-pages from git cli updating gitignore- build folder and npm folder

---
+++ build/precache-manifest.314dc4038da64f883aa3d2d37671863e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c48f95ad6e9443ac21e1499f25650d9",
+    "url": "/react-codealong-online-cloth-shop/index.html"
+  },
+  {
+    "revision": "b25c34196090620a5713",
+    "url": "/react-codealong-online-cloth-shop/static/css/main.44c0bf2f.chunk.css"
+  },
+  {
+    "revision": "702e9566ecd67cf912a3",
+    "url": "/react-codealong-online-cloth-shop/static/js/2.b1be9e24.chunk.js"
+  },
+  {
+    "revision": "61e9854a6bf57dc077cc06fd0e1da9b1",
+    "url": "/react-codealong-online-cloth-shop/static/js/2.b1be9e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25c34196090620a5713",
+    "url": "/react-codealong-online-cloth-shop/static/js/main.0c8d4ceb.chunk.js"
+  },
+  {
+    "revision": "2c24c26bc171623a9c0a",
+    "url": "/react-codealong-online-cloth-shop/static/js/runtime-main.976e44ff.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/react-codealong-online-cloth-shop/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/react-codealong-online-cloth-shop/static/media/shopping-bag.30f07071.svg"
+  }
+]);
